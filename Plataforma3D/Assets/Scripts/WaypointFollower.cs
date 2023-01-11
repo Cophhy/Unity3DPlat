f177881eb32b27b1be8f01fff519d379244e8f45
Brendha Iara Gruber de Lima <brendhaiara@hotmail.com>
2023-01-10 22:02:14 -0300

    gizzo
    add dos pontos para o movimento da plataforma

---
+++ Plataforma3D/Assets/Scripts/WaypointFollower.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointFollower : MonoBehaviour
+{
+    [SerializeField] GameObject[] waypoints; //array de gameobjects 
+    int currentWaypointIndex = 0 ;
+    [SerializeField] float speed = 1f;
+    
+    void Update()
+    {
+        //posicao do opbjeto que possui o script
+        transform.position
+    }
+}
