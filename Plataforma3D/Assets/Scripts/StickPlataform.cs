d4c2b77920fc32ce0ba280e4f7e923ca94fec95d
Brendha Iara Gruber de Lima <brendhaiara@hotmail.com>
2023-01-10 22:45:53 -0300

    TicTac
    the tictac game

---
+++ Plataforma3D/Assets/Scripts/StickPlataform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickPlataform : MonoBehaviour
+{
+    //verificar a colisao do player com a plataforma 
+    private void OnCollisionEnter(Collision collision){
+        //comparador com o nome do objeto 
+        if(collision.gameObject.name == "Player"){
+            //se true queremos que o player seja child da plataforma 
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision){
+            if(collision.gameObject.name == "Player"){
+            //se true queremos que o player seja child da plataforma 
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+
+}
