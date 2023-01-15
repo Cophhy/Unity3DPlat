068b07f35b7cd1b51ac8b49ea90f37c2f8d14a2d
Brendha Iara Gruber de Lima <brendhaiara@hotmail.com>
2023-01-14 23:02:18 -0300

    Final
    finalizado sem textura

--- Plataforma3D/Assets/Scripts/Finish.cs
+++ Plataforma3D/Assets/Scripts/Finish.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other){
         if(other.gameObject.name == "Player"){
             //terminar esse level e ir para o proximo
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             
         }
     }
